d88591acb0bd64be73c59b00a23a8a4b6a21f24f
NguyenDuy0708 <nvtduy070804@gmail.com>
2024-12-08 15:09:17 +0700

    update

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,10 +1,45 @@
 <?php
 class AdminController {
+    private $userModel;
+
+    public function __construct() {
+        $this->userModel = new User();
+    }
     public function login() {
         // Kiểm tra nếu form được gửi
-        
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $user = $this->userModel->login($username, $password);
+            if ($user) {
+                $_SESSION['user'] = $user;
+                $_SESSION['role'] = $user['role'];  
+                header('Location: index.php?controller=admin&action=index');
+                exit;
+            } else {
+                $error = "Sai tên đăng nhập hoặc mật khẩu.";
+                require_once 'views/admin/login.php';  
+                exit;
+        }
+        }
+        require_once 'views/admin/login.php';  
+    }
+    
+    public function logout() {
+        session_start();  
+        session_unset();
+        session_destroy();  
+        header('Location: index.php?controller=admin&action=login');  
+        exit;
     }
 
+    public function index() {
+        if (!isset($_SESSION['user'])) {
+            header('Location: index.php?controller=admin&action=login');
+            exit;
+        }
+        header('Location: index.php?controller=news&action=index');
+    }       
     public function dashboard() {
         // Kiểm tra nếu người dùng đã đăng nhập
         
